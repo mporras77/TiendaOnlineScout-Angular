bfb24a1c92a7db79fe1247ed46c3d4d7915d0953
mporras77 <mporras77@misena.edu.co>
2023-03-01 06:58:34 -0500

    creando-AngularTiendaScout

---
+++ src/app/app.module.ts
@@ -0,0 +1,97 @@
+/*
+
+    Programado por Luis Cabrera Benito 
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+    
+    Blog:       https://parzibyte.me/blog
+    Ayuda:      https://parzibyte.me/blog/contrataciones-ayuda/
+    Contacto:   https://parzibyte.me/blog/contacto/
+*/
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MatSliderModule} from '@angular/material/slider';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {ProductosComponent} from './productos/productos.component';
+import {ClientesComponent} from './clientes/clientes.component';
+import {VentasComponent} from './ventas/ventas.component';
+import {TiendaComponent} from './tienda/tienda.component';
+import {AgregarProductoComponent} from './agregar-producto/agregar-producto.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {FormsModule} from "@angular/forms";
+import {LoadingButtonComponent} from './loading-button/loading-button.component';
+import {TarjetaProductoComponent} from './tarjeta-producto/tarjeta-producto.component';
+import {MatCardMdImage, MatCardModule} from "@angular/material/card";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {DetalleDeProductoComponent} from './detalle-de-producto/detalle-de-producto.component';
+import {MatChipsModule} from "@angular/material/chips";
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatMenuModule} from "@angular/material/menu";
+import { TerminarCompraComponent } from './terminar-compra/terminar-compra.component';
+import {MatStepperModule} from "@angular/material/stepper";
+import { DetalleDeVentaComponent } from './detalle-de-venta/detalle-de-venta.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductosComponent,
+    ClientesComponent,
+    VentasComponent,
+    TiendaComponent,
+    AgregarProductoComponent,
+    LoadingButtonComponent,
+    TarjetaProductoComponent,
+    DetalleDeProductoComponent,
+    TerminarCompraComponent,
+    DetalleDeVentaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatInputModule,
+    FormsModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatGridListModule,
+    MatChipsModule,
+    MatBadgeModule,
+    MatMenuModule,
+    MatStepperModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
